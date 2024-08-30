8933ff3814d05b8d2b4438513f1024a95f0be8a8
codelifo <madina94495@gmail.com>
2024-08-30 16:52:10 +0600

    my Practice solution.

---
+++ script.js
@@ -0,0 +1,8 @@
+let burgerMoney = parseInt(window.prompt("Enter your Burger price: "))
+console.log(burgerMoney)
+if (burgerMoney > 500){
+    console.log("Get a Free Coke.")
+}
+else{
+    console.log("Get a 30tk.")
+}
